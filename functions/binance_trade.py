1ab4cac47ade84cace92c7dce1e853bc76f48f00
Mike - Larch Analytics <mmasters@larchanalytics.com>
2023-04-26 14:05:23 -0600

    Add files via upload

---
+++ functions/binance_trade.py
@@ -0,0 +1,13 @@
+# 4-26-2023
+# Larch Analytics
+# Written By: Michael Masters
+# mmasters@larchanalytics.com
+
+# This is the master file where you can choose which functions you would like to incorporate
+# into the trading model. You must have Binance API for it to work properly.
+
+import ascending_triangle   # provides notification on ascending triangle buy opportunity
+
+
+
+
